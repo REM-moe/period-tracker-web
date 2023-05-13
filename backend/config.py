82e96454081c1dda71d55ce801fcdbf73dd9e649
REM-moe <98999089+REM-moe@users.noreply.github.com>
2023-05-13 12:19:53 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,6 @@ import os
 
 
 class DevConfig:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root123@localhost/website'
+    SQLALCHEMY_DATABASE_URI = ''
     SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', cast = bool)
     SECRET_KEY = os.getenv("SECRET_KEY", "secret")
